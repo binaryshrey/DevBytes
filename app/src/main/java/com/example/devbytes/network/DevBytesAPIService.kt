bd6dbb22eb6826f9ddc096be6c6ac22760d84669
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2021-05-08 01:50:15 +0530

    adds APIService

---
+++ app/src/main/java/com/example/devbytes/network/DevBytesAPIService.kt
@@ -0,0 +1,31 @@
+package com.example.devbytes.network
+
+import com.example.devbytes.database.DevBytesEntity
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+
+
+private const val BASE_URL = "https://devbytes.udacity.com"
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
+interface DevBytesAPIService{
+    @GET("devbytes.json")
+    fun getProperties() : List<DevBytesEntity>
+}
+
+object DevBytesAPI {
+    val retrofitService : DevBytesAPIService by lazy {
+        retrofit.create(DevBytesAPIService::class.java)
+    }
+}
